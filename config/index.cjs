ab16ffe26b77fd5645fc2d294915235fd6b5a6c9
xtx7232 <1571177053@qq.com>
2024-07-17 15:37:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oxWaW6tzOawYa8lvJBUUIgzZh4kY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '3QhuPZ4irHzMq5YYFrIqlYDiQ6-vlebfytTEbps2kSY',
+      useTemplateId: '	INO-pwZ9AqorDMISGu9wf2h8Jaz8q5Shx_R8-gDCsHc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-14',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'DEbqPbKAxgSixJlevg8A7zdZ4TinWNyPdcpem-tfo4c',
+  CALLBACK_TEMPLATE_ID: 'LhS9rSPvDchnPAH_sK1peb2o7ZtYkLouQQYAcTJyCt4',
 
   CALLBACK_USERS: [
     {
